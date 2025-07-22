ee785fffd3d9067134c2dc6e52f6b1072adff400
Sonal Mittal <sonalmittal140103@gmail.com>
2025-07-22 13:20:22 +0530

    .

---
+++ Dynamic-Routing/dynamic/src/app/users/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserProfile = ({params} : {params : {id : string}}) => {
+    const {id} = params;
+  return (
+    <div>
+        <h1>User Profile : {id}</h1>
+    </div>
+  )
+}
+
+export default UserProfile

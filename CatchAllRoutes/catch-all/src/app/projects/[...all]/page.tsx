d4ec49acd13cd2f09dc871e74cabc7068f230a33
Sonal Mittal <sonalmittal140103@gmail.com>
2025-07-30 12:12:15 +0530

    Gaming Website

---
+++ CatchAllRoutes/catch-all/src/app/projects/[...all]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Project = async  ({params} : {params : {all : string[]}}) => {
+    const {all} = await params;
+    // console.log("--------", all);
+  return (
+    <div>
+        <h1>Project {all}</h1>  <br/> <br/>
+        All Routes {all.map(p => (
+            <li key={p}>{p}</li>
+        ))}
+    </div>
+  )
+}
+
+export default Project
